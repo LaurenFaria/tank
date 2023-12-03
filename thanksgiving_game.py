955258004e7a6043ed4216ab756877b5bcfc0817
LaurenFaria <147637258+LaurenFaria@users.noreply.github.com>
2023-12-03 12:18:53 -0500

    adding more sounds

--- thanksgiving_game.py
+++ thanksgiving_game.py
@@ -49,69 +49,85 @@ all_sprites = pygame.sprite.Group()
 
 #adding the colliding sound
 collision_sound = pygame.mixer.Sound("hurt.wav")
-
+# Load and play the background music
+pygame.mixer.music.load("background.wav")
+pygame.mixer.music.play(-1)
 
 # Main game function
 def play_game(screen, clock, bullets_group, sea_mines_group):
-    global player_score, player_lives, all_sprites
-
+    # Create a group to manage all sprites in the game
     all_sprites = pygame.sprite.Group()
+
+    # Create and add the player fish to the sprite group
     player_fish = PlayerFish()
     all_sprites.add(player_fish)
+
+    player_score = 0 #Initialize player_score before the loop
+    player_lives = 3 #Initialize player_lives
+    
+    # Images for different fish types
     fish_images = ["fishes/orange_fish1.png", "fishes/green_fish.png", "fishes/yellow_fish.png"]
 
+    # Generate initial fish objects and add them to the sprite group
     initial_fish_count = 15
     for _ in range(initial_fish_count):
-        image_path = random.choice(["fishes/orange_fish1.png", "fishes/green_fish.png", "fishes/yellow_fish.png"])
+        image_path = random.choice(fish_images)
         fish = Fishes(image_path, random.randint(1, 2))
         all_sprites.add(fish)
 
-    bullet_timer = 0
-    bullet_frequency = 500
-
-    seamine_timer = 0
-    seamine_frequency = 1000
+    # Timer variables for bullet and sea mine creation
+    bullet_timer, bullet_frequency = 0, 500
+    seamine_timer, seamine_frequency = 0, 1000
 
     while True:
+        # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             elif event.type == pygame.MOUSEBUTTONDOWN and player_lives <= 0:
-                #reset the game on mouse click if lives are zero
+                # Reset the game on mouse click if lives are zero
                 player_lives = 3
                 player_score = 0
                 all_sprites.empty()
                 bullets_group.empty()
                 sea_mines_group.empty()
 
+        # Update all sprites
         all_sprites.update()
 
-        bullet_seamine_collisions = pygame.sprite.groupcollide(bullets_group, sea_mines_group, True, False)
-
+        # Handle collisions between bullets and sea mines
+        bullet_seamine_collisions = pygame.sprite.groupcollide(bullets_group, sea_mines_group, True, True)
         for seamine, bullets in bullet_seamine_collisions.items():
-            if len(bullets) >= 5:
+            for bullet in bullets:
+                bullet.kill()
                 seamine.kill()
 
+        # Check collisions between player fish and other sprites
         collisions = pygame.sprite.spritecollide(player_fish, all_sprites, True)
         for collided_fish in collisions:
             if isinstance(collided_fish, Fishes):
-                #handels the scoring or other actions when the fish collide
+                # Handle fish collisions
                 player_score += collided_fish.value
-                # Add a new fish after collision
                 image_path = random.choice(fish_images)
-                fish = Fishes(image_path, random.randint(1,2))
-                fish.rect.x = -fish.rect.width #off-screen on the left side
-                fish.rect.y = random.randint(0, HEIGHT -fish.rect.height) #random Y position
+                fish = Fishes(image_path, random.randint(1, 2))
+                fish.rect.x = -fish.rect.width
+                fish.rect.y = random.randint(0, HEIGHT - fish.rect.height)
                 all_sprites.add(fish)
             elif isinstance(collided_fish, Seamine):
+                # Handle seamine collisions
                 player_lives -= 1
                 if player_lives <= 0:
                     display_game_over(bullets_group, sea_mines_group)
                 else:
-                    #play the collsion sound
                     collision_sound.play()
 
+        # Check collisions between sea mines and fish bullets
+        seamine_bullet_collisions = pygame.sprite.groupcollide(sea_mines_group, bullets_group, True, True)
+        for seamine in seamine_bullet_collisions:
+            seamine.kill()
+
+        # Handle other sprite collisions (e.g., between fishes and bullets)
         hits = pygame.sprite.groupcollide(all_sprites, all_sprites, False, False)
         for hit in hits:
             for target in hits[hit]:
@@ -120,32 +136,39 @@ def play_game(screen, clock, bullets_group, sea_mines_group):
                     hit.kill()
                     target.kill()
 
+        # Handle bullet shooting from the player fish
         bullet_timer += clock.get_rawtime()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and bullet_timer >= bullet_frequency:
             player_fish.shoot()
             bullet_timer = 0
 
+        # Spawn sea mines at intervals
         seamine_timer += clock.get_rawtime()
         if seamine_timer >= seamine_frequency:
             seamine = Seamine()
             all_sprites.add(seamine)
             seamine_timer = 0
 
+        # Draw everything on the screen
         screen.fill(WHITE)
         draw_background(screen)
         all_sprites.draw(screen)
         player_fish.draw(screen)
         player_fish.update()
 
+        # Display player score and lives
         font = pygame.font.Font(None, 36)
         text = font.render(f"Score: {player_score} Lives: {player_lives}", True, BLACK)
         text_rect = text.get_rect()
         text_rect.topleft = (10, 10)
         screen.blit(text, text_rect)
 
+        # Update display and maintain frame rate
         pygame.display.flip()
         clock.tick(60)
+
+
 
 
 # Function to display the introduction screen
